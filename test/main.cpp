aed4d53a8a88260cf91e916c403e3136eeaf6fdf
Sepideh Maleki <smaleki@ghostwheel2.oden.utexas.edu>
2021-07-12 14:50:56 -0500

    test fix

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "Bipart.h"
 
+using namespace phydb;
 int main(int argc, char** argv) {
-  biparting(argc, argv);
+
+  PhyDB db;
+
+  string lefFileName = argv[1];//"benchmark_1K.lef";
+  db.ReadLef(lefFileName);
+
+  string defFileName = argv[2];//"benchmark_1K.def";
+  db.ReadDef(defFileName);
+
+  int Csize = stoi(argv[3]);//25;
+  int Rsize = stoi(argv[4]);//2;
+
+  bipart::biparting(db, Csize, Rsize);
   return 0;
 };
